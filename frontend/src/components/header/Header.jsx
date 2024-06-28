ff15c7b3b4f423b3ab63ecb40402df030dd8fb02
neha sable <nehasable45@gmail.com>
2024-06-28 18:31:20 +0530

    commit

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -2,24 +2,47 @@ import React from 'react'
 import { FaLocationArrow } from "react-icons/fa";
 import { FaMapMarker } from "react-icons/fa";
 import { BsFillCalendarDateFill } from "react-icons/bs";
+import { MdOutlineWork } from "react-icons/md";
 
+import DatePicker from "react-datepicker";
+import {useState} from 'react'
+import "react-datepicker/dist/react-datepicker.css";
+import "./Header.css"
 const Header = () => {
+    const [startDate, setStartDate] = useState(new Date());
   return (
     <>
     <div className="header">
+    <h1>BOOK TICKET</h1>
         <div className="header-container">
-        <h1>BOOK TICKET</h1>
-        <div className="header-loc"></div>
-        <FaLocationArrow className='header-icon'/>
-        <input type="text"  placeholder='From'/>
-      <FaMapMarker className='header-icon' />
-      <input type="text"  placeholder='To'/>
-      <div className="header-date">
-      <BsFillCalendarDateFill />
-      <input type="text"  />
-
+        
+        <div className="header-loc">
+            <div className="loc-items">
+            <FaLocationArrow className='header-icon'/>
+            <input type="text"  placeholder='From'/>  
+            </div>
+       <div className="loc-items2">
+       <FaMapMarker className='header-icon' />
+       <input type="text"  placeholder='To'/>
+       </div>
+      
       </div>
+      <div className="header-loc">
+      <div className="header-date">
+      <BsFillCalendarDateFill className='header-icon'/>
+      <DatePicker 
+      className='date'
+      selected={startDate} onChange={(date) => setStartDate(date)} />
+      </div>
+      <div className="header-class">
+      <MdOutlineWork className='header-icon'/>
+      <input type="text" placeholder='All Classes'/>
+      </div>
+      </div>
+    
+      
         </div>
+        <button className='headerbtn'>Search</button>
     </div>
         
     </>
